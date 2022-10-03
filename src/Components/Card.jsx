6abbe251449a9db1e7d3ab691bc72cb225e197b8
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-03 18:41:03 -0300

    import de PeopleCard y PlanetsCard

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import PeopleCard from "../Components/PeopleCard.jsx";
+import PlanetsCard from "../Components/PlanetsCard.jsx";
 import image from "../Img/card_demo.jpg";
 
 const Card = () => {
