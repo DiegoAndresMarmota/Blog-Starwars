c8f1725ac2b72eb784386832dda1e55c32256926
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-03 18:40:14 -0300

    Actualización del Home.jsx

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Card from "../Components/Card";
 import "../Styles/Index.css";
 
 const Home = () => {
+  const { tienda, acciones } = useContext(Context);
   
     return (
       <>
         <div className="people mt-5">
           <div className="header-container mb-4 d-flex align-items-center">
-            <h1 className="category-title">Elenco</h1>
+            <h1 className="category-title">Personajes</h1>
             <button className="btn btn-more">
-              <Link to="/category/people">Mostrar todos</Link>
+              <Link to="/category/people">Mostrar Todos</Link>
             </button>
           </div>
           <div className="d-flex">
+            {!!tienda.caracteres &&
+              tienda.caracteres.resultados.length > 0 &&
+              tienda.caracteres.resultados
+                .filter((item, index, objeto) => index <= 3)
+                .map((item, i) => (
+                  <PeopleCard {...item} key={item.nombre} index={i} />
+                ))}
           </div>
         </div>
         <div className="vehicles mt-5">
           <div className="header-container mb-4 d-flex align-items-center">
             <h1 className="category-title">Vehiculos</h1>
             <button className="btn btn-more">
-              <Link to="/category/vehicles">Mostrar todos</Link>
+              <Link to="/category/vehicles">Mostrar Todos</Link>
             </button>
           </div>
-          <div className="d-flex">
-          </div>
+          <div className="d-flex"></div>
         </div>
         <div className="planets mt-5">
           <div className="header-container mb-4 d-flex align-items-center">
@@ -34,7 +42,13 @@ const Home = () => {
             </button>
           </div>
           <div className="d-flex">
-        
+            {!!tienda.planetas &&
+              tienda.planetas.resultados.length > 0 &&
+              tienda.planetas.resultados
+                .filter((item, index, objeto) => index <= 3)
+                .map((item, i) => (
+                  <PlanetsCard {...item} key={item.nombre} index={i} />
+                ))}
           </div>
         </div>
       </>
