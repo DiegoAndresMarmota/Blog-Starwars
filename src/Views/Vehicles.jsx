a55e05ee292940445a048b901e405cefe25b3653
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-04 18:58:53 -0300

    Views/Vehicles

---
+++ src/Views/Vehicles.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { Context } from "../Store/appContext";
+import VehicleCard from "../Components/VehicleCard.jsx";
+
+const estiloCard = {
+  width: "100%",
+};
+const estiloBoton = {
+  margin: "10px",
+};
