bd484096df6ead4564e806cf4790382e1ca7b211
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-04 19:16:10 -0300

    BrowserRouter, Routes, Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AplicationContext from "./Store/appContext";
+import Navbar from "./Components/Navbar";
+import Home from "./Views/Home";
+import People from "./Views/People";
+import Vehicles from "./Views/Vehicles";
+import Planets from "./Views/Planets";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default AplicationContext(App);
