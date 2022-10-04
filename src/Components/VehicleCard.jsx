e66ddff5b338e30bf4d15bc7be3608fe503a41e4
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-04 15:42:28 -0300

    components/VehicleCard

---
+++ src/Components/VehicleCard.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+const estiloCarta = {
+  width: "18rem",
+  margin: "5px",
+};
+
+const estiloCartaCuerpo = {
+  height: "210px",
+};
+
+const estiloBotonesComplementarios = {
+  width: "254px",
+  justifyContent: "center",
+  marginTop: "none",
+  marginBottom: "15px",
+};
+
+---------Logica del componente------------
+
+export default VehicleCard;
