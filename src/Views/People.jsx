1d38204839fc9968731856e40b9ced27a46e38cc
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-04 18:56:35 -0300

    Views/People

---
+++ src/Views/People.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useContext } from "react";
+import Card from "../Components/Card";
+import { Context } from "../Store/appContext";
+import PeopleCard from "../Components/PeopleCard.jsx";
+
+const estiloCard = {
+  width: "100%",
+};
+
+const estiloBoton = {
+  margin: "10px",
+};
