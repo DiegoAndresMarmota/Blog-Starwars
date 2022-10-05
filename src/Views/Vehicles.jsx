dc36cc3a568aa46750994feafaafa9b5d4d3703c
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-05 18:55:35 -0300

    Views/Vehiculos

--- src/Views/Vehicles.jsx
+++ src/Views/Vehicles.jsx
@@ -8,3 +8,44 @@ const estiloCard = {
 const estiloBoton = {
   margin: "10px",
 };
+
+const Vehicles = () => {
+  const { tienda, acciones } = useContext(Context);
+  return (
+    <>
+      <div className="container d-flex align-items-center justify-content-center">
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            acciones.getVehiculos(tienda?.vehiculos.atras);
+          }}
+          style={estiloBoton}
+        >
+          Retroceder página
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            acciones.getVehiculos(tienda?.vehiculos.siguiente);
+          }}
+        >
+          Siguiente página
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="card mb-3" style={estiloCard}>
+          <div className="row g-0">
+            {!!tienda.vehiculos &&
+              tienda.vehiculos.resultados.length > 0 &&
+              tienda.vehiculos.resultados.map((item, i) => (
+                <VehicleCard {...item} key={item.nombre} index={i} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Vehicles;
