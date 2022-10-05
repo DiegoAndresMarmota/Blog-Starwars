cc7f2d8346dee9f3910220c1423700616d814069
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-05 18:58:45 -0300

    views/Planetas

--- src/Views/Planets.jsx
+++ src/Views/Planets.jsx
@@ -8,3 +8,45 @@ const estiloCard = {
 const estiloBoton = {
   margin: "10px",
 };
+
+const Planets = () => {
+  const { tienda, acciones } = useContext(Context);
+
+  return (
+    <>
+      <div className="container d-flex align-items-center justify-content-center">
+        <button
+          className="btn btn-secondary"
+          onClick={() => {
+            acciones.getPlanetas(tienda?.planetas.atras);
+          }}
+          style={estiloBoton}
+        >
+          Retroceder página
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            acciones.getPlanetas(tienda?.planetas.siguiente);
+          }}
+        >
+          Siguiente página
+        </button>
+      </div>
+
+      <div className="container">
+        <div className="card mb-3" style={estiloCard}>
+          <div className="row g-0">
+            {!!tienda.planetas &&
+              tienda.planetas.resultados.length > 0 &&
+              tienda.planetas.resultados.map((item, i) => (
+                <PlanetsCard {...item} key={item.nombre} index={i} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Planets;
