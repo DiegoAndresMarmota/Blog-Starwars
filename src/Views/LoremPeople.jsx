7eede4f9ef309471d7ea707b159729b1b4bdb94f
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-06 18:57:58 -0300

    view de LoremPeople

---
+++ src/Views/LoremPeople.jsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import PeopleCard from "../Components/PeopleCard.jsx";
+import { Context } from "../Store/appContext";
+
+const estiloDiv = {
+  margin: "15px",
+};
+const estiloDiv2 = {
+  marginTop: "30px",
+  padding: "2px",
+};
+
+const estiloContainer = {
+  height: "150px",
+  width: "256px",
+};
+const estiloContainer2 = {
+  height: "200px",
+  width: "256px",
+};
