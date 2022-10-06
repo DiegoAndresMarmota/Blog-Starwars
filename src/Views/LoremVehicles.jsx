99d9ee84c5d17d5a0eed20d60245beb23f5e37d9
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-06 18:58:43 -0300

    view de LoremVehicles

---
+++ src/Views/LoremVehicles.jsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import PeopleCard from "../Components/PeopleCard.jsx";
+import { Context } from "../Store/appContext";
+
+const estiloDiv = {
+  margin: "15px",
+};
+const estiloDiv2 = {
+  marginTop: "30px",
+  padding: "2px",
+};
+const estiloContainer = {
+  height: "100px",
+  width: "213px",
+};
+const estiloContainer2 = {
+  height: "150px",
+  width: "213px",
+};
+
+const estiloImagen = {
+  height: "366px",
+  width: "640px",
+};
