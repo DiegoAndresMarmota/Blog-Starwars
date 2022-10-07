f5fb0214302ede11841e21fd3564d308cab2897b
DiegoAndresMarmota <diegoechalarp@hotmail.com>
2022-10-07 13:57:54 -0300

    Routes de LoremPeople/LoremVehicles/LoremPlanets

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import People from "./Views/People";
 import Vehicles from "./Views/Vehicles";
 import Planets from "./Views/Planets";
 import PageNotFound from "./Views/PageNotFound";
+import LoremPeople from "./Views/LoremPeople";
+import LoremVehicles from "./Views/LoremVehicles";
+import LoremPlanets from "./Views/LoremPlanets";
 import "./Styles/App.css";
 
 
@@ -22,9 +25,21 @@ function App() {
             <Route path="/categoria/personajes" element={<People />} />
             <Route path="/categoria/planetas" element={<Planets />} />
             <Route path="/categoria/vehiculos" element={<Vehicles />} />
-            <Route />
-            <Route />
-            <Route />
+            <Route path="/detalle/elenco/:index" element={<LoremPeople />} />
+            <Route
+              path="/categoria/elenco/ficha/caracter/:index"
+              element={<LoremPeople />}
+            />
+            <Route path="detalle/vehiculo/:index" element={<LoremVehicles />} />
+            <Route
+              path="/categoria/maquina/ficha/vehiculo/:index"
+              element={<LoremVehicles />}
+            />
+            <Route path="/detalle/planeta/:index" element={<LoremPlanets />} />
+            <Route
+              path="/categoria/planetas/sistema/planeta/:index"
+              element={<LoremPlanets />}
+            />
           </Routes>
         </div>
       </BrowserRouter>
